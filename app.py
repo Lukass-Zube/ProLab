50531f2a056c93bbd6892dffd109f04b093a9445
Lukass.Zube <Lukass.Zube@edu.rtu.lv>
2024-11-09 14:44:23 +0200

    .

--- app.py
+++ app.py
@@ -19,4 +19,4 @@ def prediction():
     return render_template('prediction.html', wining_team=wining_team, losing_team=losing_team)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=80, debug=True)
